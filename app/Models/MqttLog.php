7b5893790bef1df87619179d4795c6f734bcbb04
Laravel Nuxtjs <mansory.php@gmail.com>
2026-02-16 02:19:29 +0330

    chek file

--- app/Models/MqttLog.php
+++ app/Models/MqttLog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mqtt extends Model
+class MqttLog extends Model
 {
     use HasFactory;
 
