608884a7128b7759ec8fc0d26b7b5cd93b5d7b18
Laravel Nuxtjs <mansory.php@gmail.com>
2026-02-21 07:27:05 +0330

    claer

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,27 +3,24 @@
         <img :src="assets.profile" class="rounded-5 mb-3 vcard-photo" alt="mansory">
         <h4 class="fw-bold farsi2">منصوری</h4>
         <p class="text-secondary mt-3">مهندس نرم افزار | برق صنعتی و اتوماسیون</p>
-        <div class="row mt-2 justify-content-center text-center">
-            <div class="col-auto">
+        <div class="row mt-2">
+            <div class="col-4">
                 <a href="https://youtube.com/@mansorysmart"
-                   class="btn btn-outline-danger rounded-circle d-flex align-items-center justify-content-center"
-                   style="width:45px; height:45px;"
+                   class="text-danger"
                    target="_blank">
                     <i class="bi bi-youtube fs-4"></i>
                 </a>
             </div>
-            <div class="col-auto">
+            <div class="col-4">
                 <a href="https://t.me/+989153903097"
-                   class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center"
-                   style="width:45px; height:45px;"
+                   class="text-primary"
                    target="_blank">
                     <i class="bi bi-telegram fs-4"></i>
                 </a>
             </div>
-            <div class="col-auto">
+            <div class="col-4">
                 <a href="tel:09153903097"
-                   class="btn btn-outline-success rounded-circle d-flex align-items-center justify-content-center"
-                   style="width:45px; height:45px;">
+                   class="text-success">
                     <i class="bi bi-telephone fs-4"></i>
                 </a>
             </div>
@@ -31,7 +28,7 @@
         <div class="vcard-about mt-2">
             راهکارهای هوشمند صنعتی ، اتصال و مدیریت دستگاه‌ها ، جمع آوری و تحلیل داده ها بهینه‌سازی زیرساخت‌ها با فناوری نوین
         </div>
-        <div class="small mt-2">{{ time }}</div>
+        <div class="small text-danger-emphasis mt-2">{{ time }}</div>
     </div>
 </template>
 
